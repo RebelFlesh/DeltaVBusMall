99aa2a0d9816bd47fc0ff64772a3af4b4a572c7a
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-06-18 22:21:41 -0500

    product  functions

--- js/app.js
+++ js/app.js
@@ -6,31 +6,6 @@ function site(){
   loadNew();
   showResult();
 }
-
-function initialize(){
-  new Product(bag,img/bag.jpg,0,0);
-  new Product(banana,img/banana,0,0);
-  new Product(bathroom,img/bathroom.jpg,0,0);
-  new Product(boots,img/banana,0,0);
-  new Product(breakfest,img/breakfest.jpg,0,0);
-  new Product(bubblegum,img/bubblegum,0,0);
-  new Product(chair,img/chair.jpg,0,0);
-  new Product(cthulu,img/cthulu,0,0);
-  new Product(dog-duck,img/dog-duck.jpg,0,0);
-  new Product(dragon,img/dragon,0,0);
-  new Product(pen,img/pen.jpg,0,0);
-  new Product(pet-sweep,img/pet-sweep,0,0);
-  new Product(scissors,img/scissors.jpg,0,0);
-  new Product(shark,img/shark,0,0);
-  new Product(sweep,img/sweep.jpg,0,0);
-  new Product(tauntaun,img/tauntaun,0,0);
-  new Product(unicorn,img/unicorn.jpg,0,0);
-  new Product(usb,img/usb,0,0);
-  new Product(water-can,img/water-can.jpg,0,0);
-  new Product(wine-glass,img/wine-glass,0,0);
-}
-
-
 function Product(name,src,timesShown,timesChosen){
   this.name=name;
   this.src=src;
@@ -39,4 +14,37 @@ function Product(name,src,timesShown,timesChosen){
 
   Product.all.push(this);
 }
+
+function initialize(){
+  new Product("bag","img/bag.jpg",0,0);
+  new Product("banana","img/banana.jpg",0,0);
+  new Product("bathroom","img/bathroom.jpg",0,0);
+  new Product("boots","img/boots.jpg",0,0);
+  new Product("breakfest","img/breakfest.jpg",0,0);
+  new Product("bubblegum","img/bubblegum.jpg",0,0);
+  new Product("chair","img/chair.jpg",0,0);
+  new Product("cthulu","img/cthulu.jpg",0,0);
+  new Product("dog-duck","img/dog-duck.jpg",0,0);
+  new Product("dragon","img/dragon.jpg",0,0);
+  new Product("pen","img/pen.jpg",0,0);
+  new Product("pet-sweep","img/pet-sweep.jpg",0,0);
+  new Product("scissors","img/scissors.jpg",0,0);
+  new Product("shark","img/shark.jpg",0,0);
+  new Product("sweep","img/sweep.jpg",0,0);
+  new Product("tauntaun","img/tauntaun.jpg",0,0);
+  new Product("unicorn","img/unicorn.jpg",0,0);
+  new Product("usb","img/usb",0,0);
+  new Product("water-can","img/water-can.jpg",0,0);
+  new Product("wine-glass","img/wine-glass",0,0);
+  console.log(Product.all);
+  console.log(random());
+}
+
+function random(){
+  return Math.ceil(Math.random()*20);
+}
+
+
 Product.all=[];
+
+initialize();
